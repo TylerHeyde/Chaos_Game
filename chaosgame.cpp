f9b99b0b5e54e9d21072a42c1df7f7398491a918
AmritKSahota <143233086+AmritKSahota@users.noreply.github.com>
2026-04-24 22:23:44 -0700

    added in text prompting 4 user
    texted the group chat as well w/ more details -- AS

--- chaosgame.cpp
+++ chaosgame.cpp
@@ -13,21 +13,35 @@ using namespace std;
 
 int main()
 {
+	/*
+	// un comment this out to work on text font display -- AS
+	// going to try to implement the font here -- Amrit
+	Font font;
+	if (!font.loadFromFile("arial.ttf")) // this is the exact name of what I downloaded and put into x64, I tried with a few different ones; ignore those if you see them -- AS
+	{
+		cout << "Error!! NOOOO" << endl;
+		//want to keep the console open so I can read the error
+		system("pause");
+	}
+	// there was an initial error in the config which was related to an unsupported font; it was running fine before I added this in, it should be fixed now -- amrit
+	
+	Text instructions;
+	instructions.setFont(font);
+	instructions.setCharacterSize(24);
+	instructions.setFillColor(Color::White);
+	instructions.setPosition(10, 10);
+	instructions.setString("Click 3 points to start the triangle!");
+	*/
 	// Create a video mode object
 	VideoMode vm(1920, 1080);
 	// Create and open a window for the game
 	RenderWindow window(vm, "Chaos Game!!", Style::Default);
 
+	// this is just the base stuff
+
 	vector<Vector2f> vertices;
 	vector<Vector2f> points;
-// going to try to implement the font here -- Amrit
-	Font font;
-	if (!font.loadFromFile("PlaywriteNZGuides-Regular.ttf")) // this is the exact name of what I downloaded and put into x64
-	{
-		cout << "Error!! NOOOO" << endl;
-	}
-	// there was an error in the config, work this out please 
-
+	
 	while (window.isOpen())
 	{
 		/*
@@ -40,7 +54,7 @@ int main()
 		{
 			if (event.type == Event::Closed)
 			{
-				// Quit the game when the window is closed -- works
+				// Quit the game when the window is closed
 				window.close();
 			}
 			if (event.type == sf::Event::MouseButtonPressed)
@@ -54,6 +68,7 @@ int main()
 					if (vertices.size() < 3)
 					{
 						vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+						//???
 					}
 					else if (points.size() == 0)
 					{
@@ -80,7 +95,6 @@ int main()
 			///select random vertex
 			///calculate midpoint between random vertex and the last point in the vector
 			///push back the newly generated coord.
-      // this is the main math portion
 		}
 
 		/*
@@ -88,13 +102,24 @@ int main()
 		Draw
 		****************************************
 		*/
+		/*
+		window.draw(instructions);
+		//seems straightforward? -- amrit
+*/
 		window.clear();
 		for (int i = 0; i < vertices.size(); i++)
 		{
 			RectangleShape rect(Vector2f(10, 10));
 			//for an option of contributing, you could maybe change this to a circle if you want? -- Amrit
+			/*for (int i = 0; i < points.size(); i++)
+{
+    CircleShape dot(1); 
+    dot.setPosition(points[i]);
+    dot.setFillColor(Color::Red); 
+    window.draw(dot);
+}*/
 			rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
-			rect.setFillColor(Color::Magenta);
+			rect.setFillColor(Color::Red);
 			// changed the color just to test, you can change back if you want -- Amrit
 			window.draw(rect);
 		}
